728193078d31ee351788037ee8aa1d7021ed2932
Yugandhar Surya <suryayugandhar300@gmail.com>
2022-03-23 10:44:32 +0530

    added

---
+++ contracts/TodoList.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.4.22 <0.9.0;
+
+contract TodoList {
+	uint public taskCount = 0;
+
+	struct Task {
+		uint id;
+		string content;
+		bool completed;
+	}
+
+	mapping (uint => Task) public tasks;
+
+	constructor() public {
+		createTask("Say Hello to World!");
+	}
+
+	function createTask (string memory _content) public {
+		taskCount++;
+		tasks[taskCount] = Task(taskCount,_content,false);
+	}
+		
+}
